21a43372745cee5547ad4435d6ec8612e1cdf1e0
Justin Li <jli.justinli@gmail.com>
2014-12-12 21:32:11 -0500

    Initial working revision

--- tractortimer.cpp
+++ tractortimer.cpp
@@ -3,7 +3,8 @@
 #include "tractortimer.h"
 
 uint16_t display_serial = D0, display_rclk = D1, display_srclk = D2,
-         exit_in = D3, up_in = D4, set_in = D5;
+         exit_in = D3, up_in = D4, set_in = D5,
+         recv0_out = A0, recv0_in = A1, recv1_out = A4, recv1_in = A5;
 
 /*
 
@@ -36,6 +37,10 @@ uint16_t display_serial = D0, display_rclk = D1, display_srclk = D2,
 
  (todo)
 
+# receivers
+
+order: orange (A0), green (A4), blue (A6)
+
 */
 
 const uint16_t SCC_prescaler_us = (uint16_t)(SystemCoreClock / 1000000) - 1;
@@ -48,8 +53,7 @@ inline void pinLow(uint16_t pin) {
   PIN_MAP[pin].gpio_peripheral->BRR = PIN_MAP[pin].gpio_pin;
 }
 
-
-uint8_t display_segment_byte_mapping[] = {
+const uint8_t display_segment_byte_mapping[] = {
   0b01110111,
   0b01100000,
   0b00111101,
@@ -82,76 +86,129 @@ inline void write_digit(int n) {
   write_segments(display_segment_byte_mapping[n]);
 }
 
+enum States {
+  STOPPED,
+  WAITING,
+  TIMING
+};
+
 uint16_t centiseconds = 0, seconds = 0, minutes = 0;
+uint16_t last_centiseconds = 0, last_seconds = 0, last_minutes = 0;
+int state = STOPPED, display_last = 0, samples = 0, debug = 0;
+
+void increment_cs()
+{
+  centiseconds++;
+  if (centiseconds >= 100) {
+    centiseconds = 0;
+    seconds++;
+  }
+  if (seconds >= 60) {
+    seconds = 0;
+    minutes++;
+  }
+}
+
+void write_time(uint16_t centiseconds, uint16_t seconds, uint16_t minutes)
+{
+  if (debug) return;
+  pinLow(display_rclk);
+  write_digit(minutes < 10 ? 10 : minutes / 10);
+  write_digit(minutes % 10);
+  write_digit(seconds / 10);
+  write_digit(seconds % 10);
+  write_digit(centiseconds / 10);
+  write_digit(centiseconds % 10);
+  pinHigh(display_rclk);
+}
+
+void write_number(int t) {
+  pinLow(display_rclk);
+  for (int k = 0; k < 6; k++) {
+    int d = t / 100000;
+    if (k == 0 && d == 0) write_digit(10);
+    else write_digit(d);
+    t -= d * 100000;
+    t *= 10;
+  }
+  pinHigh(display_rclk);
+}
+
+int poll_receiver(int pin)
+{
+  int val = analogRead(pin);
+  if (debug) write_number(val);
+  return val > 1000;
+}
+
+void poll_receivers()
+{
+  if (state == STOPPED) return;
+
+  if (state == TIMING && display_last == 0 && !poll_receiver(recv1_in)) {
+    last_minutes = minutes;
+    last_seconds = seconds;
+    last_centiseconds = centiseconds;
+    display_last = 200;
+  } else if (state == WAITING && !poll_receiver(recv0_in)) {
+    state = TIMING;
+    samples = 0;
+  }
+}
 
 extern "C" void TIM4_irq_handler(void)
 {
   if (TIM_GetITStatus(TIM4, TIM_IT_Update) != RESET)
   {
     TIM_ClearITPendingBit(TIM4, TIM_IT_Update);
-
-    centiseconds++;
-    if (centiseconds >= 100) {
-      centiseconds = 0;
-      seconds++;
-    }
-    if (seconds >= 60) {
-      seconds = 0;
-      minutes++;
+    if (state == TIMING) {
+      increment_cs();
     }
 
-    pinLow(display_rclk);
-    write_digit(minutes / 10);
-    write_digit(minutes % 10);
-    write_digit(seconds / 10);
-    write_digit(seconds % 10);
-    write_digit(centiseconds / 10);
-    write_digit(centiseconds % 10);
-    pinHigh(display_rclk);
+    if (display_last) {
+      if (display_last > 0) display_last--;
+      write_time(last_centiseconds, last_seconds, last_minutes);
+    } else {
+      write_time(centiseconds, seconds, minutes);
+    }
   }
+}
 
-  // if (TIM_GetITStatus(TIM4, TIM_IT_CC2) != RESET)
-  // {
-  //   TIM_ClearITPendingBit(TIM4, TIM_IT_CC2);
-  // }
+extern "C" void TIM3_irq_handler(void)
+{
+  if (TIM_GetITStatus(TIM3, TIM_IT_Update) != RESET)
+  {
+    TIM_ClearITPendingBit(TIM3, TIM_IT_Update);
+    poll_receivers();
+  }
+}
+
+void finalize_time()
+{
+  state = STOPPED;
+  display_last = -1;
 }
 
 void loop()
 {
-  // Counter
-  // for (int i = 0; i <= 199999; i++) {
-  //   if (digitalRead(exit_in) == LOW) break;
-  //   if (digitalRead(set_in) == HIGH) i = 0;
-  //   if (digitalRead(up_in) == HIGH) i = 100000;
-  //   pinLow(display_rclk);
-  //   int t = i;
-  //   for (int k = 0; k < 6; k++) {
-  //     int d = t / 100000;
-  //     if (k == 0 && d == 0) write_digit(10);
-  //     else write_digit(d);
-  //     t -= d * 100000;
-  //     t *= 10;
-  //   }
-  //   pinHigh(display_rclk);
-  //   delay(10);
-  // }
+  if (digitalRead(exit_in) == LOW) {
+    finalize_time();
+  }
+  if (digitalRead(set_in) == HIGH) {
+    centiseconds = seconds = minutes = 0;
+    state = WAITING;
+    samples = 0;
+    display_last = 0;
+  }
+  if (digitalRead(up_in) == HIGH) debug = !debug;
 
-  // FAIL
-  // for (int i = 0; i < 6; i++) {
-  //   pinLow(display_rclk);
-  //   write_digit(10);
-  //   write_digit2(i < 2 ? i : i == 5 ? 2 : i + 2);
-  //   write_digit(11);
-  //   write_digit(12);
-  //   write_digit(13);
-  //   write_digit(14);
-  //   pinHigh(display_rclk);
-  //   delay(50);
-  // }
+  pinHigh(recv0_out);
+  pinHigh(recv1_out);
 }
 
 void setup()
 {
+  Wiring_TIM3_Interrupt_Handler = TIM3_irq_handler;
   Wiring_TIM4_Interrupt_Handler = TIM4_irq_handler;
 
   pinMode(display_rclk, OUTPUT);
@@ -161,7 +218,21 @@ void setup()
   pinMode(up_in, INPUT);
   pinMode(set_in, INPUT);
 
+  pinMode(recv0_out, OUTPUT);
+  pinMode(recv0_in, INPUT);
+  pinMode(recv1_out, OUTPUT);
+  pinMode(recv1_in, INPUT);
+
+  RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM3, ENABLE);
   RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM4, ENABLE);
+
+  TIM_TimeBaseInitTypeDef poll_timer;
+  poll_timer.TIM_Prescaler = SCC_prescaler_us;
+  poll_timer.TIM_CounterMode = TIM_CounterMode_Up;
+  poll_timer.TIM_Period = 1000;
+  poll_timer.TIM_ClockDivision = TIM_CKD_DIV1;
+  poll_timer.TIM_RepetitionCounter = 0;
+  TIM_TimeBaseInit(TIM3, &poll_timer);
 
   TIM_TimeBaseInitTypeDef timer;
   timer.TIM_Prescaler = SCC_prescaler_us;
@@ -171,6 +242,14 @@ void setup()
   timer.TIM_RepetitionCounter = 0;
   TIM_TimeBaseInit(TIM4, &timer);
 
+  NVIC_InitTypeDef poll_nvic;
+  poll_nvic.NVIC_IRQChannel = TIM3_IRQn;
+  poll_nvic.NVIC_IRQChannelPreemptionPriority = 0;
+  poll_nvic.NVIC_IRQChannelSubPriority = 1;
+  poll_nvic.NVIC_IRQChannelCmd = ENABLE;
+  NVIC_Init(&poll_nvic);
+  TIM_Cmd(TIM3, ENABLE);
+
   NVIC_InitTypeDef nvic;
   nvic.NVIC_IRQChannel = TIM4_IRQn;
   nvic.NVIC_IRQChannelPreemptionPriority = 0;
@@ -179,6 +258,7 @@ void setup()
   NVIC_Init(&nvic);
   TIM_Cmd(TIM4, ENABLE);
 
+  TIM_ITConfig(TIM3, TIM_IT_Update, ENABLE);
   TIM_ITConfig(TIM4, TIM_IT_Update, ENABLE);
   // TIM_ITConfig(TIM4, TIM_IT_CC2, ENABLE);
 }
